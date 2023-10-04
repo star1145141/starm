4b1e1c0ab76ec4c4572e51874412128a36d32aaf
star1145141 <144141863+star1145141@users.noreply.github.com>
2023-10-04 09:36:57 +0800

    ok

--- src/content/Moditem.java
+++ src/content/Moditem.java
@@ -1,4 +1,9 @@
 package content;
 
 public class Moditem {
+    AWA=new void item("AWA",Color.valueOF("d99d73")){{
+    hardness=1;
+    cost=0.5f;
+        boolean alwaysUnlocked = true;
+    }};
 }
