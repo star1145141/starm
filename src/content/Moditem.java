5be5743bcab1ced03c5075217dd35a93713fa8d4
star1145141 <144141863+star1145141@users.noreply.github.com>
2023-10-24 22:30:43 +0800

    new

--- src/content/Moditem.java
+++ src/content/Moditem.java
@@ -4,16 +4,15 @@ package content;
 
 
 public class Moditem {
-    public static item;
+    public static Moditem;
 
     public static <item> void load(item newitem) {
         AWA = newitem;){
             {
-                int hardness = 1;
+                int hardness = (int) 1f;
                 boolean alwaysUnlocked = true;
                 float cost = 0.5f;
             }
         }
-        ;
     }
 }
